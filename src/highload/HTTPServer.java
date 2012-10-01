8e48f58b6e3b0380f2f01c7d1a8d1a4cf4e49fa8
Ivan Remen <bhyk@yandex.ru>
2012-10-01 21:46:59 +0400

    Default constructor added for HTTPServer

--- src/highload/HTTPServer.java
+++ src/highload/HTTPServer.java
@@ -1,8 +1,39 @@
 package highload;
 
-public final class HTTPServer 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+
+public final class HTTPServer implements Runnable 
 {
-	private int numberOfWorkers = 5;
-	private String address = "0.0.0.0";
-	private int listeningPort = 8080;
+	private final int defaultNumberOfWorkers = 5;
+	private final int defaultListeningPort = 8080;
+	private final String defaultAddress = "0.0.0.0";
+	private final int defaultBacklog = 255;
+	
+	private int numberOfWorkers;
+	private String address;
+	private int listeningPort;
+	private int backLog;
+	
+	private ServerSocket socket;
+
+	@Override
+	public void run() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public HTTPServer() throws UnknownHostException, IOException
+	{
+		numberOfWorkers = defaultNumberOfWorkers;
+		address = defaultAddress;
+		listeningPort = defaultListeningPort;
+		backLog = defaultBacklog;
+		
+		socket = new ServerSocket(listeningPort, 0, InetAddress.getByName(defaultAddress));
+	}	
+	
 }
