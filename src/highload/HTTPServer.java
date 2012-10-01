5d564f82e62bb89834970d1fc2215647f80ac19a
Ivan Remen <bhyk@yandex.ru>
2012-10-01 21:19:05 +0400

    numberOfWorkers, address, and port added to HTTPServer

--- src/highload/HTTPServer.java
+++ src/highload/HTTPServer.java
@@ -1,5 +1,8 @@
 package highload;
 
-public final class HTTPServer {
-
+public final class HTTPServer 
+{
+	private int numberOfWorkers = 5;
+	private String address = "0.0.0.0";
+	private int listeningPort = 8080;
 }
